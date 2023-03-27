06418da31d68f33d6f444b90854060e25236124d
Pascal Visser <pvisser@bioinf.nl>
2023-03-27 13:52:33 +0200

    created shell script for Trimmomatic installation

---
+++ software/installation_scripts/Trimmomatic_install.sh
@@ -0,0 +1,5 @@
+ #!/bin/sh 
+#Trimmomatic 0.39
+wget -O Trimmomatic-0.39.zip http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip 
+unzip Trimmomatic-0.39.zip -d software
+rm Trimmomatic-0.39.zip 
