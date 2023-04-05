dced1804a8b091b4ee03bbc2b8d1ff176bffaa38
Pascal Visser <pvisser@bioinf.nl>
2023-04-05 15:42:09 +0200

    Simplified installion flow

--- scripts/R/Library.R
+++ scripts/R/Library.R
@@ -2,36 +2,32 @@
 
 start_time <- Sys.time()
 
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
 
-# check if DESeq2 package exist
-is_DESeq_installed <- require("DESeq2")
+if (!require("getopt")) {
+  BiocManager::install("DESeq2")
+}
 
+if (!require("getopt")) {
+  BiocManager::install("RColorBrewer")
+}
 
-# check if RColorBrewer package exist
-is_RColorBrewer_installed <- require("RColorBrewer")
+if (!require("getopt")) {  
+  BiocManager::install("gplots")
+}
 
-# check if gplots package exist
-is_gplots_installed <- require("gplots")
+if (!require("getopt")) {
+  install.packages("getopt")
+}
 
+if (!require("optparse")) {
+  install.packages("optparse")
+}
 
-# check if data.frame package exist
-is_data.frame_installed <- require("data.frame")
-
-
-# check if optparse package exist
-is_optparse_installed <- require("optparse")
-
-# check if getopt package exist
-is_getopt_installed <- require("getopt")
-
-
-cat("is_DESeq_installed =", is_DESeq_installed, "\n")
-cat("is_RColorBrewer_installed = ", is_RColorBrewer_installed, "\n")
-cat("is_getopt_installed = ", is_getopt_installed, "\n")
-cat("is_data.frame_installed = ", is_data.frame_installed, "\n")
-cat("is_optparse_installed =", is_optparse_installed, "\n")
-cat("is_getopt_installed =", is_getopt_installed, "\n")
-
+if (!require("data.table")) {
+  install.packages("data.table")
+}
 
 end_time <- Sys.time()
 
