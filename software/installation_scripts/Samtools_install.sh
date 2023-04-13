56d2cc070286134c71dd138c54584b7f1f521bff
Pascal Visser <pvisser@bioinf.nl>
2023-04-13 08:54:58 +0200

    Changed datapath of PATH

--- software/installation_scripts/Samtools_install.sh
+++ software/installation_scripts/Samtools_install.sh
@@ -2,5 +2,5 @@
 #Samtools 1.11
 wget -O samtools.tar.bz2 https://sourceforge.net/projects/samtools/files/samtools/1.11/samtools-1.11.tar.bz2/download
 tar -xf samtools.tar.bz2 -C software
-export PATH=$PATH:Dataprocessing_eindopdracht/software/samtools-0.1.11
+export PATH=$PATH:Dataprocessing_final/software/samtools-0.1.11
 rm samtools.tar.bz2
