7acb9993b938fd7c6c916d0a4f8b02bd6fba3767
Pascal Visser <pvisser@bioinf.nl>
2023-04-13 08:59:12 +0200

    Changed documentation

--- software/installation_scripts/Bowtie2_install.sh
+++ software/installation_scripts/Bowtie2_install.sh
@@ -1,5 +1,5 @@
  #!bin/sh
-#Bowtie2 2.4.2
+#Bowtie2 2.5.1
 wget -O bowtie2.zip https://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.5.1/bowtie2-2.5.1-linux-x86_64.zip/download 
 unzip bowtie2.zip -d software
 export PATH=$HOME/Dataprocessing_final/software/bowtie2-2.5.1-linux-x86_64:$PATH
